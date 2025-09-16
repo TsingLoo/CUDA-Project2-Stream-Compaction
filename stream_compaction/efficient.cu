39a2277d17256134cd22bcf79b7824aa06bf0a2d
Xiaonan <tsingloo@outlook.com>
2025-09-16 15:07:18 -0400

    optimize the scan()

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -50,30 +50,48 @@ namespace StreamCompaction {
 			idata[k - halfStride] = parentValue ;
 			idata[k] = parentValue + originalLeftChildValue;
         }
-
-        void scanWithoutTimer(int n, int* odata, const int* idata) {
+        
+        void scan_device(int n, int* dev_odata, const int* dev_idata) {
             int paddedN = 1 << ilog2ceil(n);
             dim3 fullBlocksPerGrid((paddedN + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
-            int* dev_idata;
+            int* dev_temp; // Use a temporary buffer for the in-place algorithm
+            cudaMalloc((void**)&dev_temp, paddedN * sizeof(int));
+            cudaMemset(dev_temp, 0, paddedN * sizeof(int));
+            cudaMemcpy(dev_temp, dev_idata, n * sizeof(int), cudaMemcpyDeviceToDevice);
 
-            cudaMalloc((void**)&dev_idata, paddedN * sizeof(int));
-            cudaMemset(dev_idata, 0, paddedN * sizeof(int));
+            // Up-Sweep
+            for (int d = 0; d < ilog2ceil(n); d++) {
+                kernWorkEfficientUpSweep << <fullBlocksPerGrid, BLOCK_SIZE >> > (paddedN, n, d, dev_temp);
+            }
+
+            setLastElementToZero << <1, 1 >> > (paddedN, dev_temp);
+
+            // Down-Sweep
+            for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
+                kernWorkEfficientDownSweep << <fullBlocksPerGrid, BLOCK_SIZE >> > (paddedN, n, d, dev_temp);
+            }
+
+            cudaMemcpy(dev_odata, dev_temp, n * sizeof(int), cudaMemcpyDeviceToDevice);
+
+            cudaFree(dev_temp);
+        }
+
+        void scanWithoutTimer(int n, int* odata, const int* idata) {
+            int* dev_idata;
+            int* dev_odata;
+
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+
             cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
 
-            for (int d = 0; d <= ilog2ceil(n) - 1; d++) {
-                kernWorkEfficientUpSweep << <fullBlocksPerGrid, BLOCK_SIZE >> > (paddedN, n, d, dev_idata);
-            }
+            scan_device(n, dev_odata, dev_idata);
 
-            setLastElementToZero << <1, 1 >> > (paddedN, dev_idata);
-
-            for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
-                kernWorkEfficientDownSweep << <fullBlocksPerGrid, BLOCK_SIZE >> > (paddedN, n, d, dev_idata);
-            }
-
-            cudaMemcpy(odata, dev_idata, n * sizeof(int), cudaMemcpyDeviceToHost);
+            cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
 
             cudaFree(dev_idata);
+            cudaFree(dev_odata);
         }
 
         /**
@@ -139,22 +157,28 @@ namespace StreamCompaction {
 
             //compute the temporary array
 			kernComputeCompactFlag<< <fullBlocksPerGrid, BLOCK_SIZE >> > (n, dev_flags, dev_idata);
-            cudaMemcpy(odata, dev_flags, n * sizeof(int), cudaMemcpyDeviceToHost);
 
 			//compute the scan of the temporary array
-            int* host_scanResult = new int[n];
-            scanWithoutTimer(n, host_scanResult, odata);
-            cudaMemcpy(dev_scanResult, host_scanResult, n * sizeof(int), cudaMemcpyHostToDevice);
+            scan_device(n, dev_scanResult, dev_flags);
 
 			//compute the scatter
-            int totalCount = host_scanResult[n - 1] + odata[n - 1];
+            int lastElementOfScan = 0;
+            int lastElementOfFlags = 0;
+            cudaMemcpy(&lastElementOfScan, &dev_scanResult[n - 1], sizeof(int), cudaMemcpyDeviceToHost);
+            cudaMemcpy(&lastElementOfFlags, &dev_flags[n - 1], sizeof(int), cudaMemcpyDeviceToHost);
+
+
+            int totalCount = lastElementOfScan + lastElementOfFlags;
             cudaMalloc((void**)&dev_odata, totalCount * sizeof(int));
             kernCompactScatter<< <fullBlocksPerGrid, BLOCK_SIZE >> > (n, dev_odata, dev_idata, dev_flags, dev_scanResult);
             
             cudaMemcpy(odata, dev_odata, totalCount * sizeof(int), cudaMemcpyDeviceToHost);
 
-
+            cudaFree(dev_idata);
             cudaFree(dev_flags);
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
+
 
             timer().endGpuTimer();
             return totalCount;
